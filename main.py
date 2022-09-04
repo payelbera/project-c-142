4d0b233abca46677e964dd502a6c9008273818d4
payelbera <60781680+payelbera@users.noreply.github.com>
2022-09-04 10:51:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,47 +38,10 @@ def unliked_article():
         "status": "success"
     }), 201
 
-@app.route("/popular-articles")
-def popular_articles():
-    article_data = []
-    for article in output:
-        _d = {
-            "url": article[0],
-            "title": article[1],
-            "text": article[2],
-            "lang": article[3],
-            "total_events": article[4]
-        }
-        article_data.append(_d)
-    return jsonify({
-        "data": article_data,
-        "status": "success"
-    }), 200
+#write the API for the popular articles
 
-@app.route("/recommended-articles")
-def recommended_articles():
-    all_recommended = []
-    for liked_article in liked_articles:
-        output = get_recommendations(liked_article[4])
-        for data in output:
-            all_recommended.append(data)
-    import itertools
-    all_recommended.sort()
-    all_recommended = list(all_recommended for all_recommended,_ in itertools.groupby(all_recommended))
-    article_data = []
-    for recommended in all_recommended:
-        _d = {
-            "url": recommended[0],
-            "title": recommended[1],
-            "text": recommended[2],
-            "lang": recommended[3],
-            "total_events": recommended[4]
-        }
-        article_data.append(_d)
-    return jsonify({
-        "data": article_data,
-        "status": "success"
-    }), 200
+#write the API for the recommemded articles
+
 
 if __name__ == "__main__":
     app.run()
